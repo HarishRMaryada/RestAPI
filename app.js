b3979e732dc226f1b1f7f6a3dcc2d83b10d8c864
Harish R M <harishmaryada@outlook.com>
2018-08-23 22:06:02 +0530

    Added Middleware

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require('express')
-const app = express();
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+const app = express()
+
+var requestTime = function (req, res, next) {
+  req.requestTime = Date.now()
+  next()
+}
+
+app.use(requestTime)
+
+app.get('/', function (req, res) {
+  var data = ""+req.requestTime
+  res.send(data)
+})
+
+
+
+app.listen(7979, () => console.log('Example app listening on port 7979!'))
