023a9d1b146731f35e7334459bbecf3a20b8c333
Harish R M <harishmaryada@outlook.com>
2018-08-28 22:49:15 +0530

    Adding Directory-Path for Version

--- Controller/index.js
+++ Controller/index.js
@@ -1,12 +1,40 @@
 const express = require( 'express')
 const router = express.Router()
-router.use(function (req, res, next) {
-    next()
-})
 
-router.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
-})
+const _ = require('lodash')
+const filesystem  = require('fs')
+const path = require('path')
+// router.use(function (req, res, next) {
+//     next()
+// })
+//
+// router.use(function (err, req, res, next) {
+//   console.error(err.stack)
+//   res.status(500).send('Something broke!')
+// })
 
-module.exports = router
+
+// function getCurrentVersionFolder(srcpath){
+//   return filesystem.readdirSync(srcpath).filter(function (file) {
+//         return filesystem.statSync(path.join(srcpath, file)).isDirectory();
+//     });
+// }
+
+
+findFile = function(dir)
+{
+  let dirs = filesystem.readdirSync(dir)
+  let files = _.forEach(dirs,function(file){
+      console.log(file);
+  })
+    return files
+};
+var list = findFile(__dirname);
+
+
+var controller = function(){
+
+  //let dirs = findFile(__dirname)
+}
+
+module.exports = controller
